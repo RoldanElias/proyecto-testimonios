27a37dafa785fb6a4a52339a61cad8e1ce6ee764
RoldanElias <eliasrol97@gmail.com>
2022-05-26 16:36:12 -0300

    Proyecto completado

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -7,7 +7,7 @@ function Testimonio(props) {
       <img
         className='imagen-testimonio'
         src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-        alt='Foto de Emma' />
+        alt='Foto' />
       <div className='contenedor-texto-testimonio'>
         <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
         <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
